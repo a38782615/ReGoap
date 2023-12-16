80181042d8b04c7a3c6f3bfa1e3e174f961c8013
a38782615 <38782615@qq.com>
2023-12-16 11:41:16 +0800

    修复了一些问题 打了一些日志

---
+++ Assets/ReGoap/Unity/FSMExample/Goals/CollectResourceGoal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ReGoap.Core;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
+
+namespace ReGoap.Unity.FSMExample.Goals
+{
+    public class CollectResourceGoal : ReGoapGoal<string, object>
+    {
+        public string ResourceName;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            goal.Set("collectedResource" + ResourceName, true);
+            goal.Set("reconcilePosition", true);
+        }
+
+        public override float GetPriority()
+        {
+            return Priority;
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("GoapGoal('{0}', '{1}')", Name, ResourceName);
+        }
+    }
+}
+
